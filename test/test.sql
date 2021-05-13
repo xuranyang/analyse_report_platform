957abe3c97de44ffd9eeda4c48d94d81d95170ce
xuranyang <362438997@qq.com>
2021-05-13 13:50:50 +0800

    add sql

---
+++ test/test.sql
@@ -0,0 +1,6 @@
+INSERT INTO django_test.app_demo_dayrechargeuseramount(create_date,recharge_user_num,recharge_amount_num) VALUES
+('2021-05-01~2021-05-07',30,3000),
+('2021-05-08~2021-05-14',20,2000),
+('2021-05-15~2021-05-21',25,6000),
+('2021-05-22~2021-05-28',28,4000),
+('2021-05-29~2021-06-04',28,2500);
