1841e101141b6dfed57b1bf31eb20602aa1b8136
xuranyang <362438997@qq.com>
2021-05-18 12:05:09 +0800

    新增 网页登录验证

--- analyse_report_platform/urls.py
+++ analyse_report_platform/urls.py
@@ -17,10 +17,20 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from app_demo import views as app_demo_views
+from django.contrib.auth.decorators import login_required
+from django.urls import include
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', app_demo_views.index, name="index"),
     url(r'^analyse_report/$', app_demo_views.get_analyse_demo, name="demo1"),
     url(r'^demo/$', app_demo_views.get_big_screen, name="demo2"),
+    # url(r'^login/$', app_demo_views.login, name="login"),
+    # url(r'^login/$', LoginView.as_view, name="login"),
+    url(r'^logout/$', app_demo_views.user_logout, name="logout"),
+    url(r'^login/$', LoginView.as_view(template_name='login.html'), name="login"),
+    # url(r'^login_check/$', app_demo_views.login_check, name="login_check"),
+
 ]
--- app_demo/views.py
+++ app_demo/views.py
@@ -5,10 +5,17 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.db import connections
+from django.contrib import auth
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'index.html')
+    # return render(request, 'login.html')
 
 
 def get_analyse_demo(request):
@@ -32,5 +39,37 @@ def get_analyse_demo(request):
                    'recharge_amount_num_lst': recharge_amount_num_lst})
 
 
+@login_required(login_url='/login')
 def get_big_screen(request):
     return render(request, 'big_screen.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user_obj = auth.authenticate(username=username, password=password)
+    if user_obj:
+        return render(request, 'login.html')
+    else:
+        return render(request, 'login.html', {'error_msg': '密码错误'})
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+# @login_required(login_url='/login')
+# def login_check(request):
+#     if request.method == "POST":
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user_obj = auth.authenticate(username=username, password=password)
+#
+#         if user_obj:
+#             # login(request, user_obj)
+#             # return redirect("/demo/")
+#             return render(request, 'big_screen.html')
+#         else:
+#             return redirect("/login")
+#             # return render(request, 'login.html', {'error_msg': '密码错误'})
